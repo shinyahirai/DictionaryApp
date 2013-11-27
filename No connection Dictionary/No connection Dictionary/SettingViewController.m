6d69ab8e595a599a39546827aa9fce6e5a969548
shinyahirai <908.shinya@gmail.com>
2013-11-27 22:14:03 +0800

    Fixed status bar 2.
    Fixed status bar 2.

--- No connection Dictionary/No connection Dictionary/SettingViewController.m
+++ No connection Dictionary/No connection Dictionary/SettingViewController.m
@@ -45,17 +45,17 @@
     navTitle.rightBarButtonItem = btn1;
     [navBar pushNavigationItem:navTitle animated:YES];
     
-    // ステータスバーの文字色
-    // [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-    
     [self.view addSubview:navBar];
     
+    // ステータスバーの文字色をViewごとに切り替える
     if ([self respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]) {
         //viewControllerで制御することを伝える。iOS7 からできたメソッド
         [self setNeedsStatusBarAppearanceUpdate];
     }
 }
 
+#pragma mark -
+#pragma mark status bar
 - (BOOL)prefersStatusBarHidden {
     //YESでステータスバーを非表示（NOなら表示）
     return NO;
